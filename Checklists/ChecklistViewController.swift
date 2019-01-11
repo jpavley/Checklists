8a3d07fdd00a83d028de7d37bb79255499aab5b9
John Pavley <john@pavley.com>
2019-01-10 19:14:32 -0500

    implemented numberOfRowsInSection and cellForRowAt

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -14,6 +14,17 @@ class ChecklistViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    // MARK:- TableView Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
 
 
 }
