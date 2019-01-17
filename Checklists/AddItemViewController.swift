6105b99167547b3c111e83b1998bf751333e5ce1
John Pavley <john@pavley.com>
2019-01-17 19:33:53 +0100

    wired up cancel and done buttons

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -12,5 +12,15 @@ class AddItemViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
     }
+    
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+    }
+
 }
