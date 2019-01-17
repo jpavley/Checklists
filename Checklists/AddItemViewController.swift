6778af9e0c5e0f18fdb68f04caa51888427fb01a
John Pavley <john@pavley.com>
2019-01-17 19:28:53 +0100

    AddItemViewController

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by John Pavley on 1/17/19.
+//  Copyright © 2019 John Pavley. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
