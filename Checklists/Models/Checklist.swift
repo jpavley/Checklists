5926b9e299a61b94e0997666d1b5c11efed60f1c
John Pavley <john@pavley.com>
2019-01-25 08:19:45 -0500

    added new model: Checklist, which is equitable and codable

---
+++ Checklists/Models/Checklist.swift
@@ -0,0 +1,13 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by John Pavley on 1/25/19.
+//  Copyright © 2019 John Pavley. All rights reserved.
+//
+
+import UIKit
+
+class Checklist: NSObject, Codable {
+
+}
