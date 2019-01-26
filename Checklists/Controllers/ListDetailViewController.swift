96d046992759d53518e6c4c433ac863ff22ada22
John Pavley <john@pavley.com>
2019-01-25 19:33:33 -0500

    completed the static ListDetailViewController class and delegate

--- Checklists/Controllers/ListDetailViewController.swift
+++ Checklists/Controllers/ListDetailViewController.swift
@@ -6,4 +6,85 @@
 //  Copyright © 2019 John Pavley. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol ListDetailViewControllerDelegate: class {
+    
+    func listDetailViewControllerDidCancel(_ controller: ListDetailViewController)
+    
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishAdding checklist: Checklist)
+    
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishEditing checklist: Checklist)
+}
+
+class ListDetailViewController: UITableViewController, UITextFieldDelegate {
+    
+    // MARK:- Outlets
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    // MARK:- Properties
+    
+    weak var delegate: ListDetailViewControllerDelegate?
+    var checklistToEdit: Checklist?
+    
+    // MARK:- View Management
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let checklist = checklistToEdit {
+            title = "Edit Checklist"
+            textField.text = checklist.name
+            doneBarButton.isEnabled = true
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    // MARK:- Actions
+    
+    @IBAction func cancel() {
+        delegate?.listDetailViewControllerDidCancel(self)
+    }
+    
+    @IBAction func done() {
+        if let checklist = checklistToEdit {
+            
+            checklist.name = textField.text!
+            delegate?.listDetailViewController(self, didFinishEditing: checklist)
+        } else {
+            
+            let checklist = Checklist(name: textField.text!)
+            delegate?.listDetailViewController(self, didFinishAdding: checklist)
+        }
+    }
+    
+    // MARK:- Table View Delegates
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        // don't allow any row to be selected
+        return nil
+    }
+    
+    // MARK:- Text Field Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        
+        doneBarButton.isEnabled = false
+        return true
+    }
+}
