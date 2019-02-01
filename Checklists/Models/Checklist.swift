98502fb0181b93177ff8ba4f069693be23b401b6
John Pavley <john@pavley.com>
2019-01-31 20:03:45 -0500

    three ways to count unchecked items: for in loop, reduce, and filter

--- Checklists/Models/Checklist.swift
+++ Checklists/Models/Checklist.swift
@@ -18,12 +18,20 @@ class Checklist: NSObject, Codable {
         super.init()
     }
     
+//    func countUncheckedItems() -> Int {
+//        var count = 0
+//        for item in items where !item.checked {
+//            count += 1
+//        }
+//
+//        return count
+//    }
+    
+//    func countUncheckedItems() -> Int {
+//        return items.reduce(0) { c, item in c + (item.checked ? 0 : 1) }
+//    }
+    
     func countUncheckedItems() -> Int {
-        var count = 0
-        for item in items where !item.checked {
-            count += 1
-        }
-        
-        return count
+        return items.filter { !$0.checked }.count
     }
 }
