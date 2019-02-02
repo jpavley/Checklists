d1451809da483d85dbf5dc9fc4e50a61a5db7d48
John Pavley <john@pavley.com>
2019-02-02 12:20:16 -0500

    IconPickerViewController and IconPickerViewControllerDelegate

---
+++ Checklists/Controllers/IconPickerViewController.swift
@@ -0,0 +1,39 @@
+//
+//  IconPickerViewController.swift
+//  Checklists
+//
+//  Created by John Pavley on 2/2/19.
+//  Copyright © 2019 John Pavley. All rights reserved.
+//
+
+import UIKit
+
+protocol IconPickerViewControllerDelegate: class {
+    func iconPicker(_ picker: IconPickerViewController,
+                    didPick iconName: String)
+}
+
+class IconPickerViewController: UITableViewController {
+    weak var delegate: IconPickerViewControllerDelegate?
+    
+    let icons = [ "No Icon", "Appointments", "Birthdays", "Chores", "Drinks", "Folder", "Groceries", "Inbox", "Photos", "Trips" ]
+    
+    // MARK:- Table View Delegates
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return icons.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IconCell", for: indexPath)
+        let iconName = icons[indexPath.row]
+        
+        cell.textLabel!.text = iconName
+        cell.imageView!.image = UIImage(named: iconName)
+        
+        return cell
+    }
+}
+
