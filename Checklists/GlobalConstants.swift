0ceddc1d99b54173b988e303c92c5fc3f768f4f2
John Pavley <john@pavley.com>
2019-02-10 10:47:55 -0500

    struct to organize constants and magic numbers

---
+++ Checklists/GlobalConstants.swift
@@ -0,0 +1,18 @@
+//
+//  GlobalConstants.swift
+//  Checklists
+//
+//  Created by John Pavley on 2/10/19.
+//  Copyright © 2019 John Pavley. All rights reserved.
+//
+
+import Foundation
+
+typealias GK = GlobalConstants
+
+struct GlobalConstants {
+    
+    struct Index {
+        static let noIndex = -1
+    }
+}
