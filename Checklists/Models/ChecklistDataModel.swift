faaff23e875da3d9ee991632dc5fd640e751e48f
John Pavley <john@pavley.com>
2019-02-10 11:45:14 -0500

    found one last place to use a GK

--- Checklists/Models/ChecklistDataModel.swift
+++ Checklists/Models/ChecklistDataModel.swift
@@ -33,7 +33,7 @@ class ChecklistDataModel {
     func registerDefaults() {
         // if ChecklistIndex is -1 then no checklist was opened
         // if FirstTime is true then this is the first time the user has launced the app
-        let dictionary = ["ChecklistIndex": GK.Index.noIndex, GK.Key.firstTime: true] as [String : Any]
+        let dictionary = [GK.Key.checkListIndex: GK.Index.noIndex, GK.Key.firstTime: true] as [String : Any]
         UserDefaults.standard.register(defaults: dictionary)
     }
     
