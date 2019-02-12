c41d71f5605ecd87fbe2843e4b1c3638ea6c26bb
John Pavley <john@pavley.com>
2019-02-12 18:52:58 -0500

    if the due date is in the futyure print a notification

--- Checklists/Models/ChecklistItem.swift
+++ Checklists/Models/ChecklistItem.swift
@@ -29,4 +29,10 @@ class ChecklistItem: NSObject, Codable {
     func toggleChecked() {
         checked = !checked
     }
+    
+    func scheduleNotification() {
+        if shouldRemind && dueDate > Date() {
+            print("We should schedule a notification!")
+        }
+    }
 }
