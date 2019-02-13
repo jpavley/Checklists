fd5b2985271547957b62ac47b6f1abbe75b87a66
John Pavley <john@pavley.com>
2019-02-13 09:40:15 -0500

    added should remind toggled action to ask again for notification privileges of needed

--- Checklists/Controllers/ItemDetailViewController.swift
+++ Checklists/Controllers/ItemDetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 protocol ItemDetailViewControllerDelegate: class {
     
@@ -88,6 +89,18 @@ class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
     @IBAction func dateChanged(_ datePicker: UIDatePicker) {
         dueDate = datePicker.date
         updateDueDateLabel()
+    }
+    
+    @IBAction func shouldRemindToggled(_ switchControl: UISwitch) {
+        textField.resignFirstResponder()
+        
+        if switchControl.isOn {
+            let center = UNUserNotificationCenter.current()
+            center.requestAuthorization(options: [.alert, .sound]) {
+                granted, error in
+                // do nothing
+            }
+        }
     }
     
     // MARK:- Helper Methods
